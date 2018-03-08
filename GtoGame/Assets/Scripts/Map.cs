53fdd740d6646c428d868ab6a6f3865d03ddc4a9
Unknown <wtstas@gmail.com>
2018-03-08 11:21:34 +0100

    added mapscript and asset

---
+++ GtoGame/Assets/Scripts/Map.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class Map : MonoBehaviour
+    {
+        public GameObject Hex;
+        public float AddedHexes;
+        public float Scale;
+        public float Startwidth;
+        private List<GameObject> hexList = new List<GameObject>();
+        private const float XHexDifference = 0.866025404f;
+        private const float ZHexDifference = 1.5f;
+        private float _row;
+        private float _hexNumber;
+        private float _totalHexes;
+        private float _yScaleDifference;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            System.Random rand = new System.Random();
+            for (int i = 0; i < Startwidth; i++)
+            {
+                _totalHexes += Startwidth + i * AddedHexes;
+            }
+
+            Hex.transform.localScale = new Vector3(Scale, Scale , Scale);
+            for (int i = 0; i < _totalHexes; i++)
+            {
+                if (_hexNumber >= Startwidth + AddedHexes * _row )
+                {
+                    _hexNumber = 0;
+                    _row++;
+                }
+
+                GameObject oppositHex = Hex.gameObject;
+                GameObject currentHex = Hex.gameObject;
+                float random = rand.Next(0, 100);
+                if (random > 66)
+                {
+                    currentHex.transform.localScale = new Vector3(Scale, Scale, Scale *2);
+                    oppositHex.transform.localScale = new Vector3(Scale, Scale, Scale *2);
+                    _yScaleDifference = Scale/2;
+                }
+                else
+                {
+                    currentHex.transform.localScale = new Vector3(Scale, Scale, Scale);
+                    oppositHex.transform.localScale = new Vector3(Scale, Scale, Scale);
+                    _yScaleDifference = 0;
+                }
+
+            
+                currentHex.transform.SetPositionAndRotation(new Vector3((_hexNumber * 2 * XHexDifference + XHexDifference * -AddedHexes * _row) * Scale, _yScaleDifference, ZHexDifference * _row * Scale), Quaternion.Euler(new Vector3(90, 0, 0)));
+                Instantiate(currentHex);
+                hexList.Add(currentHex);
+
+                if (i <= _totalHexes - (Startwidth + (Startwidth - 1) *AddedHexes))
+                {     
+                    oppositHex.transform.SetPositionAndRotation(new Vector3((_hexNumber * 2 * XHexDifference + XHexDifference * -AddedHexes * _row) * Scale, _yScaleDifference, ZHexDifference * (2 * Startwidth - _row - 2) * Scale), Quaternion.Euler(new Vector3(90, 0, 0)));
+                    Instantiate(oppositHex);
+                    hexList.Add(oppositHex);
+                }
+                _hexNumber++;
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

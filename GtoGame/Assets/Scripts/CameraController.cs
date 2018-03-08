176f886917a0fab172aeb9bc0845d026334ecef6
Unknown <valkenburg1997@gmail.com>
2018-03-08 20:25:34 +0100

    Added camera movement with mouse and WASD

---
+++ GtoGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+
+    public Vector2 panLimit;
+    public float scrollSpeed = 20f;
+
+    public float minY =0f;
+    public float maxY=40f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 position = transform.position;
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            position.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            position.z -= panSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey("d")||Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            position.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            position.x -= panSpeed * Time.deltaTime;
+        }
+
+        position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
+        position.z = Mathf.Clamp(position.z, -panLimit.y, panLimit.y);
+        position.y = Mathf.Clamp(position.y, minY, maxY);
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        position.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+
+        transform.position = position;
+    }
+}

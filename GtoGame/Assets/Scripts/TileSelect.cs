cb9dcf42e156355a6010a98b848d6980bb390ad8
Unknown <wtstas@gmail.com>
2018-03-08 16:10:12 +0100

    added main scene, mapgeneration, raycast

---
+++ GtoGame/Assets/Scripts/TileSelect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TileSelect : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit raycast = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycast);
+                if (hit)
+                {
+                    Destroy(raycast.transform.gameObject);
+                }
+            }
+        }
+    }
+}

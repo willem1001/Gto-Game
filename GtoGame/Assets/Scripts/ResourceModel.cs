17228d6add06a2713fc3754d15395239674ef9a4
Unknown <wtstas@gmail.com>
2018-03-08 12:41:22 +0100

    stuff

---
+++ GtoGame/Assets/Scripts/ResourceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts
+{
+    public class ResourceModel : MonoBehaviour
+    {
+        private string Name { get; set; }
+        private string Discription { get; set; }
+        private float Amount { get; set; }
+
+        event EventHandler<Resource> 
+    }
+}

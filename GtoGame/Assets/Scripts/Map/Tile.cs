68397dc5ebc374412bdd99ec21e2c4622824ba54
Unknown <valkenburg1997@gmail.com>
2018-03-09 09:17:24 +0100

    Added hasChild in tile.cs

---
+++ GtoGame/Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public bool hasChild()
+    {
+        return this.gameObject.transform.childCount > 0;
+    }
+
+}

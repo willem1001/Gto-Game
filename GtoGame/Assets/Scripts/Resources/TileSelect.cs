22060713fab7734033f8b8262948e5c958985daa
Unknown <valkenburg1997@gmail.com>
2018-03-17 11:15:45 +0100

    Added selected unit highlighting (cant deselect yet)

--- GtoGame/Assets/Scripts/Resources/TileSelect.cs
+++ GtoGame/Assets/Scripts/Resources/TileSelect.cs
@@ -7,18 +7,18 @@ namespace Assets.Scripts
     {
         public UnitFactory Factory;
         private bool _inBuildMode;
-
+        private GameObject selectedObject = null;
 
         void Update()
         {
-            if (!_inBuildMode) return;
-            if (!Input.GetMouseButtonDown(0)) return;
+            // if (!_inBuildMode) return;
+            // if (!Input.GetMouseButtonDown(0)) return;
             RaycastHit raycast;
-            var hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycast);
-            if (!hit) return;
-            {
-                Factory.SpawnUnit(raycast.transform.gameObject);
-            }
+            // var hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycast);
+            // if (!hit) return;
+            // {
+            //
+            // }
             //else if (Input.GetMouseButtonDown(1) && _select != null)
             //{
             //    RaycastHit raycast;
@@ -29,8 +29,62 @@ namespace Assets.Scripts
             //            _select.gameObject.transform.GetChild(0).gameObject);
             //    }
             //}
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycast))
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    Factory.SpawnUnit(raycast.transform.gameObject);
+                }
+                if (Input.GetMouseButton(1))
+                {
+                    Debug.Log("selected: " + raycast.collider.name);
+                    selectedObject = raycast.transform.gameObject;
+                    SelectObject(selectedObject);
+                }
+                else
+                {
+                    ClearSelection();
+                }
+            }
+
+        }
+        void SelectObject(GameObject obj)
+        {
+            if (selectedObject != null)
+            {
+                if (obj == selectedObject)
+                    return;
+
+                ClearSelection();
+            }
+
+            selectedObject = obj;
+
+            Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in rs)
+            {
+                Material m = r.material;
+                m.color = Color.green;
+                r.material = m;
+            }
         }
 
+        void ClearSelection()
+        {
+            if (selectedObject == null)
+                return;
+
+            Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in rs)
+            {
+                Material m = r.material;
+                m.color = Color.white;
+                r.material = m;
+            }
+
+
+            selectedObject = null;
+        }
         public void EnterBuildMode()
         {
             _inBuildMode = true;

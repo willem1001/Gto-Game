48cb9eab56d25f22c1e5f6bc795a5b62c94f3366
Unknown <valkenburg1997@gmail.com>
2018-03-28 20:05:22 +0200

    Added  camera facing hpbar to unit

---
+++ GtoGame/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera camera;
+
+    public void Update()
+    {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward,
+            camera.transform.rotation * Vector3.up);
+    }
+}

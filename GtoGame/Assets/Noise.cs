89981073acdf0fec1b82b9a73ae6eda01631503a
Unknown <valkenburg1997@gmail.com>
2018-05-14 10:50:18 +0200

    Added water background

---
+++ GtoGame/Assets/Noise.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour
+{
+
+    public float power = 3;
+    public float scale = 1;
+    public float timeScale = 1;
+
+    private float offsetX;
+    private float offsetY;
+    private MeshFilter meshFilter;
+
+    // Use this for initialization
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        MakeNoise();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakeNoise();
+        offsetX += Time.deltaTime * timeScale;
+        offsetY += Time.deltaTime * timeScale;
+    }
+
+    void MakeNoise()
+    {
+        Vector3[] vertices = meshFilter.mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = CalculateHeight(vertices[i].x, vertices[i].z) * power;
+        }
+
+        meshFilter.mesh.vertices = vertices;
+    }
+
+    float CalculateHeight(float x, float y)
+    {
+        float xCord = x * scale + offsetX;
+        float yCord = y * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCord, yCord);
+    }
+}
